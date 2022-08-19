1b9078cc1a1cbbe78a3b32d7f7d7c713a751a4d3
simranjit singh <simranjits008@gmail.com>
2022-08-19 20:31:42 +0530

    update contatct route

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -15,7 +15,8 @@ let transporter = nodemailer.createTransport({
     pass: process.env.PASSWORD,
     clientId: process.env.OAUTH_CLIENTID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    // refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    accessToken: process.env.OAUTH_ACCESS_TOKEN
   },
 });
 
