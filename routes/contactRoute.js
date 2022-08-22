628a81bed592fd897fa0da4d96b0c650266f351a
simranjit singh <simranjits008@gmail.com>
2022-08-22 18:44:29 +0530

    nodemailer update

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -2,10 +2,23 @@ const express = require("express");
 const router = express.Router();
 require("dotenv").config();
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 const { check, validationResult } = require("express-validator");
 
 const Contact = require("../schema/contactSchema");
+
+const oauth2Client = new OAuth2(
+  process.env.OAUTH_CLIENTID,
+  process.env.OAUTH_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground",
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+});
+const accessToken = oauth2Client.getAccessToken();
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
@@ -15,8 +28,8 @@ let transporter = nodemailer.createTransport({
     pass: process.env.PASSWORD,
     clientId: process.env.OAUTH_CLIENTID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    // refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-    accessToken: process.env.OAUTH_ACCESS_TOKEN
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    accessToken: accessToken,
   },
 });
 
@@ -81,7 +94,7 @@ router.post(
       console.error(err.message);
       res.status(500).send("Server Error");
     }
-  }
+  },
 );
 
 //@route    PUT api/contacts/update/:id
